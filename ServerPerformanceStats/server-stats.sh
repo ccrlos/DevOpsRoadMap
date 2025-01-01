826801d66af72d61c2c06e6011e0eeb4ecb6c1b3
ccrlos <159073203+ccrlos@users.noreply.github.com>
2025-01-01 22:05:30 +0100

    Update server-stats.sh

--- ServerPerformanceStats/server-stats.sh
+++ ServerPerformanceStats/server-stats.sh
@@ -2,3 +2,7 @@
 
 #1.Total CPU usage
 echo "\e[31m Total CPU usage ==> \e[0m" "\e[32m $(top -bn1 | awk '/Cpu/ {print}') \e[0m"
+
+#2.Total memory usage (Free vs Used including percentage)
+echo "Total Memory (Usage) ==> " $(free -g | awk '/Mem/ {printf "%.2f%%\n", $3 / $2 * 100}') "Total Memory (Free) ==> " $(free -g | awk '/Mem/ {printf "%.2f%%\n", $4 / $2 * 100}')
+
