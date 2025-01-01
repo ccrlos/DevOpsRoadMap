62b1c9264308fb11122856f03b80cda1ab2520ef
ccrlos <159073203+ccrlos@users.noreply.github.com>
2025-01-01 22:34:29 +0100

    Update server-stats.sh

--- ServerPerformanceStats/server-stats.sh
+++ ServerPerformanceStats/server-stats.sh
@@ -6,3 +6,11 @@ echo "\e[31m Total CPU usage ==> \e[0m" "\e[32m $(top -bn1 | awk '/Cpu/ {print}'
 #2.Total memory usage (Free vs Used including percentage)
 echo "Total Memory (Usage) ==> " $(free -g | awk '/Mem/ {printf "%.2f%%\n", $3 / $2 * 100}') "Total Memory (Free) ==> " $(free -g | awk '/Mem/ {printf "%.2f%%\n", $4 / $2 * 100}')
 
+#3.Total disk usage (Free vs Used including percentage)
+echo "Total disk usage (Usage) ==> " $(df -h --total | awk '/total/ {printf "%.2f%%\n", $3 / $2 * 100}') "Total disk usage (Free) ==> " $(df -h --total | awk '/total/ {printf "%.2f%%\n", $4 / $2 >
+
+#4.Top 5 processes by CPU usage
+echo "Top 5 processes by CPU usage ==> " $(ps -Ao user,uid,comm,pid,pmem,tty --sort=-pcpu | head -n 6)
+
+#5.Top 5 processes by Memory usage
+echo "Top 5 processes by Memory usage ==> " $(ps -Ao user,uid,comm,pid,pmem,tty --sort=-pmem | head -n 6)
